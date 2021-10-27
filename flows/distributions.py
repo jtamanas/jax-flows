3599cdb256cfc3f05e88eafe9a339c7e7d586382
John Tamanas <8213449+jtamanas@users.noreply.github.com>
2021-10-27 11:37:29 -0700

    Added context embedding

--- flows/distributions.py
+++ flows/distributions.py
@@ -2,6 +2,7 @@ import jax.numpy as np
 from jax import random
 from jax.scipy.special import logsumexp
 from jax.scipy.stats import norm, multivariate_normal
+from jax.experimental import stax
 
 
 def Normal():
@@ -49,13 +50,47 @@ def GMM(means, covariances, weights):
     return init_fun
 
 
-def Flow(transformation, prior=Normal()):
+def MLPEmbedding(
+    rng,
+    input_dim,
+    embedding_dim=32,
+    hidden_dim=128,
+    num_layers=2,
+    act=None,
+    use_context_embedding=True,
+):
+    assert (
+        use_context_embedding
+    ), "Initializing embedding with use_context_embedding==False"
+
+    if act is None:
+        act = stax.Selu
+
+    layers = [lyr for _ in range(num_layers) for lyr in (stax.Dense(hidden_dim), act)]
+    layers += [stax.Dense(embedding_dim)]
+    init_random_params, embed = stax.serial(*layers)
+    _, init_params = init_random_params(rng, (-1, input_dim))
+    return init_params, embed
+
+
+def Flow(
+    transformation,
+    prior=Normal(),
+    context_embedding_kwargs=None,
+):
     """
     Args:
         transformation: a function mapping ``(rng, input_dim)`` to a
             ``(params, direct_fun, inverse_fun)`` triplet
         prior: a function mapping ``(rng, input_dim)`` to a
             ``(params, log_pdf, sample)`` triplet
+        context_kwargs:
+            Additional keyword arguments to pass to the context embedding function.
+            use_context_embedding: whether to use the context embedding
+            embedding_dim: the dimension of the context embedding (output_dim)
+            hidden_layers: Number of hidden layers in the context embedding.
+            hidden_dim: Number of hidden units in the hidden layers.
+            act: Activation function to use in the hidden layers.
 
     Returns:
         A function mapping ``(rng, input_dim)`` to a ``(params, log_pdf, sample)`` triplet.
@@ -68,27 +103,70 @@ def Flow(transformation, prior=Normal()):
         ...     flows.Reverse()
         ... )
         >>> init_fun = flows.Flow(transformation, Normal())
-        >>> params, log_pdf, sample = init_fun(rng, input_dim)
+        >>> flow_params, log_pdf, sample = init_fun(rng, input_dim)
     """
 
-    def init_fun(rng, input_dim, context_dim=None, hidden_dim=64):
-        transformation_rng, prior_rng = random.split(rng)
+    assert "embedding_dim" in context_embedding_kwargs, "Must specify embedding_dim"
+    assert (
+        "use_context_embedding" in context_embedding_kwargs
+    ), "Must specify use_context_embedding"
 
-        params, direct_fun, inverse_fun = transformation(
-            transformation_rng, input_dim, context_dim=context_dim, hidden_dim=hidden_dim,
+    def init_fun(rng, input_dim, context_dim=None, hidden_dim=64):
+        if (
+            context_embedding_kwargs["embedding_dim"] is None
+            or not context_embedding_kwargs["use_context_embedding"]
+        ):
+            embedding_dim = context_dim
+        else:
+            embedding_dim = context_embedding_kwargs["embedding_dim"]
+
+        transformation_rng, embedding_rng, prior_rng = random.split(rng, 3)
+
+        if context_embedding_kwargs["use_context_embedding"]:
+            embedding_params, context_embedding = MLPEmbedding(
+                embedding_rng, context_dim, **context_embedding_kwargs
+            )
+        else:
+            embedding_params = None
+            context_embedding = None
+
+        flow_params, direct_fun, inverse_fun = transformation(
+            transformation_rng,
+            input_dim,
+            context_dim=embedding_dim,
+            hidden_dim=hidden_dim,
         )
+
+        params = (embedding_params, flow_params)
+
         prior_params, prior_log_pdf, prior_sample = prior(prior_rng, input_dim)
 
-        def log_pdf(params, inputs, context=None):
-            u, log_det = direct_fun(params, inputs, context=context)
+        def embed(embedding_params, context):
+            if context is not None and context_embedding is not None:
+                assert embedding_params is not None, "embedding_params must be provided"
+                context = context_embedding(embedding_params, context)
+            return context
+
+        def log_pdf(params, inputs, context=None, embedding_params=None):
+            embedding_params, flow_params = params
+
+            context = embed(embedding_params, context)
+            u, log_det = direct_fun(flow_params, inputs, context=context)
             log_probs = prior_log_pdf(prior_params, u)
             return log_probs + log_det
 
-        def sample(rng, params, context=None, num_samples=1):
+        def sample(rng, params, context=None, num_samples=None, embedding_params=None):
+            assert (
+                num_samples is not None or context is not None
+            ), "Only one of num_samples or context must be specified"
             if context is not None:
                 num_samples = context.shape[0]
+
+            embedding_params, flow_params = params
+
+            context = embed(embedding_params, context)
             prior_samples = prior_sample(rng, prior_params, num_samples)
-            return inverse_fun(params, prior_samples, context=context)[0]
+            return inverse_fun(flow_params, prior_samples, context=context)[0]
 
         return params, log_pdf, sample
 
